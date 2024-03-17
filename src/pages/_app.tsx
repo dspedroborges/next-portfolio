5072dac4f92d376792495d081bc725b965e5edf6
wdpedroborges <xpedrostewart@gmail.com>
2024-03-17 19:05:52 -0300

    atualização nos links de contato

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <div className="w-[75px] h-[75px] rounded-full fixed bottom-5 right-5 animate-ping bg-green-300 z-50"></div>
-      <a href="https://wa.me/5561998746331?text=Olá, Pedro%21" target="_blank">
+      <Link href="https://wa.me/5561998746331?text=Olá, Pedro%21" target="_blank">
         <img
           src="/whatsapp.png"
           alt="Whatsapp logo"
@@ -14,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
           height={75}
           className="fixed bottom-5 right-5 cursor-pointer hover:scale-110 transition-all z-50"
         />
-      </a>
+      </Link>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { FiExternalLink } from "react-icons/fi";
 import { VscChromeMaximize } from "react-icons/vsc";
 import { FaRegWindowMinimize } from "react-icons/fa";
 import { MdOutlineClose } from "react-icons/md";
+import Link from "next/link";
 
 type OptionType = {
   name: string;
@@ -38,10 +39,6 @@ const projects = [
   }
 ];
 
-// VscChromeMaximize
-// FaRegWindowMinimize
-// MdOutlineClose
-
 export default function Home() {
   return (
     <div className="bg-gray-950 pt-16" style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' viewBox='0 0 192 192'%3E%3Cpath fill='%23353535' fill-opacity='0.44' d='M192 15v2a11 11 0 0 0-11 11c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H145v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11 13 13 0 1 1 .02 26 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43a6.1 6.1 0 0 0-3.03 4.87V143h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 181 164a11 11 0 0 0 11 11v2a13 13 0 0 1-13-13 12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84a6.1 6.1 0 0 0-4.87-3.03H145v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 124 181a11 11 0 0 0-11 11h-2a13 13 0 0 1 13-13c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43a6.1 6.1 0 0 0 3.03-4.87V145h-35.02a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 107 124a11 11 0 0 0-22 0c0 1.94 1.16 4.75 2.53 6.11l2.36 2.36a6.93 6.93 0 0 1 1.22 7.56l-.43.84a8.08 8.08 0 0 1-6.66 4.13H49v35.02a6.1 6.1 0 0 0 3.03 4.87l.84.43c1.58.79 4 .4 5.24-.85l2.36-2.36a12.04 12.04 0 0 1 7.51-3.11A13 13 0 0 1 81 192h-2a11 11 0 0 0-11-11c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V145H11.98a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 0 1 0 177v-2a11 11 0 0 0 11-11c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H47v-35.02a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 28 109a13 13 0 1 1 0-26c2.47 0 5.79 1.37 7.53 3.11l2.36 2.36a4.94 4.94 0 0 0 5.24.85l.84-.43A6.1 6.1 0 0 0 47 84.02V49H11.98a8.08 8.08 0 0 1-6.66-4.13l-.43-.84a6.91 6.91 0 0 1 1.22-7.56l2.36-2.36A10.06 10.06 0 0 0 11 28 11 11 0 0 0 0 17v-2a13 13 0 0 1 13 13c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84A6.1 6.1 0 0 0 11.98 47H47V11.98a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 68 11 11 11 0 0 0 79 0h2a13 13 0 0 1-13 13 12 12 0 0 1-7.53-3.11l-2.36-2.36a4.93 4.93 0 0 0-5.24-.85l-.84.43A6.1 6.1 0 0 0 49 11.98V47h35.02a8.08 8.08 0 0 1 6.66 4.13l.43.84a6.91 6.91 0 0 1-1.22 7.56l-2.36 2.36A10.06 10.06 0 0 0 85 68a11 11 0 0 0 22 0c0-1.94-1.16-4.75-2.53-6.11l-2.36-2.36a6.93 6.93 0 0 1-1.22-7.56l.43-.84a8.08 8.08 0 0 1 6.66-4.13H143V11.98a6.1 6.1 0 0 0-3.03-4.87l-.84-.43c-1.59-.8-4-.4-5.24.85l-2.36 2.36A12 12 0 0 1 124 13a13 13 0 0 1-13-13h2a11 11 0 0 0 11 11c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V47h35.02a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 179 28a13 13 0 0 1 13-13zM84.02 143a6.1 6.1 0 0 0 4.87-3.03l.43-.84c.8-1.59.4-4-.85-5.24l-2.36-2.36A12 12 0 0 1 83 124a13 13 0 1 1 26 0c0 2.47-1.37 5.79-3.11 7.53l-2.36 2.36a4.94 4.94 0 0 0-.85 5.24l.43.84a6.1 6.1 0 0 0 4.87 3.03H143v-35.02a8.08 8.08 0 0 1 4.13-6.66l.84-.43a6.91 6.91 0 0 1 7.56 1.22l2.36 2.36A10.06 10.06 0 0 0 164 107a11 11 0 0 0 0-22c-1.94 0-4.75 1.16-6.11 2.53l-2.36 2.36a6.93 6.93 0 0 1-7.56 1.22l-.84-.43a8.08 8.08 0 0 1-4.13-6.66V49h-35.02a6.1 6.1 0 0 0-4.87 3.03l-.43.84c-.79 1.58-.4 4 .85 5.24l2.36 2.36a12.04 12.04 0 0 1 3.11 7.51A13 13 0 1 1 83 68a12 12 0 0 1 3.11-7.53l2.36-2.36a4.93 4.93 0 0 0 .85-5.24l-.43-.84A6.1 6.1 0 0 0 84.02 49H49v35.02a8.08 8.08 0 0 1-4.13 6.66l-.84.43a6.91 6.91 0 0 1-7.56-1.22l-2.36-2.36A10.06 10.06 0 0 0 28 85a11 11 0 0 0 0 22c1.94 0 4.75-1.16 6.11-2.53l2.36-2.36a6.93 6.93 0 0 1 7.56-1.22l.84.43a8.08 8.08 0 0 1 4.13 6.66V143h35.02z'%3E%3C/path%3E%3C/svg%3E")` }}>
@@ -65,9 +62,9 @@ export default function Home() {
         <div className="mb-8 p-2">
           <p>&gt; Pedro.infoContato</p>
           <p>[
-            "<a href="#" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">tpedroborges@gmail.com</a>",
-            "<a href="#" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">Linkedin</a>",
-            "<a href="#" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">Github</a>",
+            "<Link href="mailto:tpedroborges@gmail.com" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">tpedroborges@gmail.com</Link>",
+            "<Link href="https://www.linkedin.com/in/pedroborges11/" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">Linkedin</Link>",
+            "<Link href="https://github.com/wdpedroborges" target="_blank" className="text-blue-300 hover:text-blue-400 hover:underline">Github</Link>",
             ]
           </p>
         </div>
@@ -77,7 +74,7 @@ export default function Home() {
         </div>
         <div className="mb-8 p-2">
           <p>&gt; Pedro.interesses</p>
-          <p>["acoustic guitar", "juggling"]</p>
+          <p>["violão", "malabarismo"]</p>
         </div>
         <div className="mb-8 p-2">
           <p>&gt; Pedro.educacao</p>
@@ -117,10 +114,26 @@ export default function Home() {
 
         <div className="flex justify-center pt-12 pb-28 px-2 lg:px-12">
           <ul className="flex flex-col justify-center items-start gap-4">
-            <li className="text-green-400 text-center"><span className="font-bold"><BsWhatsapp className="inline" /> Whatsapp: </span><span>(61) 9 9874-6331</span></li>
-            <li className="text-green-400 text-center"><span className="font-bold"><BsEnvelope className="inline" /> Email: </span><span>tpedroborges@gmail.com</span></li>
-            <li className="text-green-400 text-center"><span className="font-bold"><BsLinkedin className="inline" /> Linkedin: </span><span>pedroborges11</span></li>
-            <li className="text-green-400 text-center"><span className="font-bold"><BsGithub className="inline" /> Github: </span><span>wdpedroborges</span></li>
+            <li className="text-green-400 text-center">
+              <Link className="hover:underline" href="https://wa.me/5561998746331?text=Olá, Pedro%21" target="_blank">
+                <span className="font-bold"><BsWhatsapp className="inline" /> Whatsapp: </span><span>(61) 9 9874-6331</span>
+              </Link>
+            </li>
+            <li className="text-green-400 text-center">
+              <Link className="hover:underline" href="mailto:tpedroborges@gmail.com" target="_blank">
+                <span className="font-bold"><BsEnvelope className="inline" /> Email: </span><span>tpedroborges@gmail.com</span>
+              </Link>
+            </li>
+            <li className="text-green-400 text-center">
+              <Link className="hover:underline" href="https://www.linkedin.com/in/pedroborges11/" target="_blank">
+                <span className="font-bold"><BsLinkedin className="inline" /> Linkedin: </span><span>pedroborges11</span>
+              </Link>
+            </li>
+            <li className="text-green-400 text-center">
+              <Link className="hover:underline" href="https://github.com/wdpedroborges" target="_blank">
+                <span className="font-bold"><BsGithub className="inline" /> Github: </span><span>wdpedroborges</span>
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
